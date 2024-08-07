4c62308ac26d6f89d34fe4d400b1b246f2d664b8
Surya Sarafe <surya.akdhan@gmail.com>
2024-08-07 22:10:29 +0700

    fix config unoptimize image for ghpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
